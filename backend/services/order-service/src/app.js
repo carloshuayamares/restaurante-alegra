222f207c2df03a006223fb647dc7eb4c6ffb75c0
Carlos Alberto Huayamares De La Cruz <carlos.huayamares.d@gmail.com>
2024-10-13 10:19:14 -0500

    feat - valor de los puertos en variables env v1

--- backend/services/order-service/src/app.js
+++ backend/services/order-service/src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors'); // Importar CORS
 const axios = require('axios');
 const app = express();
-const port = 3001;
+const port = process.env.PORT;
 
 app.use(cors()); // Habilitar CORS
 app.use(express.json());
