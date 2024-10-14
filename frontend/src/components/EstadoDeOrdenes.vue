03d9d0ca31c67438c02a4ad8bbf37872a1f16ec3
Carlos Alberto Huayamares De La Cruz <carlos.huayamares.d@gmail.com>
2024-10-14 13:39:36 -0500

    feat - actualizacion frontend v3.0

--- frontend/src/components/EstadoDeOrdenes.vue
+++ frontend/src/components/EstadoDeOrdenes.vue
@@ -1,22 +1,11 @@
 <template>
     <div>
         <h2>Estados de Órdenes</h2>
-        <v-container>
-            <v-data-table
-                :headers="headers"
-                :items="items"
-                class="elevation-1"
-            >
-                <template v-slot:top>
-                    <v-toolbar flat>
-                        <v-toolbar-title>Tabla de Ejemplo</v-toolbar-title>
-                        <v-divider class="mx-4" inset vertical></v-divider>
-                    </v-toolbar>
-                </template>
-
-
-            </v-data-table>
-        </v-container>
+        <v-data-table 
+            :headers="headers"
+            :items="orders"
+            class="elevation-1">
+        </v-data-table>
     </div>
 </template>
 
@@ -28,22 +17,15 @@ export default {
     data() {
         return {
             headers: [
-                { text: 'Indice Tiempo', value: 'orderId' },
-                { text: 'OrdendelPedido', value: 'ID-ORDER' },
-                { text: 'Nombre del Pedido', value: 'name' },
-                { text: 'Estatus del Pedido', value: 'status' },
+                { text: 'IndiceTiempo', key: 'orderId' },
+                { text: 'OrdendelPedido', key: 'ID-ORDER' },
+                { text: 'NombredelPedido', key: 'name' },
+                { text: 'EstatusdelPedido', key: 'status' },
             ],
-            items: [],
+            orders: [],
         };
     },
-    methods: {
-        editItem(item) {
-        console.log("Editar: ", item);
-        },
-        deleteItem(item) {
-        console.log("Eliminar: ", item);
-        }
-    },
+
     async created() {
         try {
             const response = await axios.post('http://localhost:3001/api/statusOrders');
@@ -58,7 +40,7 @@ export default {
             })
             console.log({ orders })
 
-            this.items = orders;
+            this.orders = orders;
         } catch (error) {
             console.log(error)
             alert('Failed to consulted orders registration.');
